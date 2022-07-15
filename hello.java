af0e9e42b144307ecac8f01a20878f918020419e
Jibyungsun <sosoda90@naver.com>
2022-07-15 16:30:12 +0900

    add 1 line

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public clss Hello{
-	public static void main(String[]args){
-		System.out.println("Hello! World!);
+     public static void main(String[]args){
+	System.out.println("Hello! World!");
+	System.out.println("Bye!");
 	}
 }
